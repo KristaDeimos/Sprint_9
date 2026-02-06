f957b456bb976f97f6a23437f4560af0fb3d6ea7
KristaDeimos <kristavoitishkina@yandex.ru>
2026-02-07 00:56:19 +0300

    Delete pages/__init__.py

--- pages/__init__.py
+++
@@ -1,8 +0,0 @@
-from login_page import LoginPage
-
-class LoginPage:
-    def __init__(self, driver):
-        self.driver = driver
-    
-    def login(self, username, password):
-        return self
