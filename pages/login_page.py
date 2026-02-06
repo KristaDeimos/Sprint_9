04b5f66c3501a9f28064ebd8bb0dea5cefb25f4e
KristaDeimos <kristavoitishkina@yandex.ru>
2026-02-07 00:10:52 +0300

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,41 +1,28 @@
-from selenium.webdriver.common.by import By
+import allure
 from .base_page import BasePage
+from locators.login_locators import LoginLocators
 
 
 class LoginPage(BasePage):
-    
-    # Локаторы
-    EMAIL_INPUT = (By.NAME, "email")
-    PASSWORD_INPUT = (By.NAME, "password")
-    LOGIN_BUTTON = (By.XPATH, "//button[contains(text(), 'Войти')]")
-    REGISTER_LINK = (By.XPATH, "//a[contains(text(), 'Создать аккаунт')]")
-    LOGOUT_BUTTON = (By.XPATH, "//button[contains(text(), 'Выход')]")
-    AUTH_FORM = (By.CLASS_NAME, "form__container")
-    ERROR_MESSAGE = (By.CLASS_NAME, "form__error")
-    
     def __init__(self, driver):
         super().__init__(driver)
-        self.open("/signin")
-    
-    def is_auth_form_displayed(self):
-        return self.is_element_displayed(self.AUTH_FORM)
-    
+        self.locators = LoginLocators()
+
+    def fill_email(self, email):
+        self.send_keys(self.locators.EMAIL_INPUT, email, "Ввод email")
+
+    def fill_password(self, password):
+        self.send_keys(self.locators.PASSWORD_INPUT, password, "Ввод пароля")
+
+    def click_login_button(self):
+        self.click_element(self.locators.LOGIN_BUTTON, "Нажатие кнопки входа")
+
     def login(self, email, password):
-        self.input_text(self.EMAIL_INPUT, email)
-        self.input_text(self.PASSWORD_INPUT, password)
-        self.click_element(self.LOGIN_BUTTON)
-    
-    def go_to_registration(self):
-        self.click_element(self.REGISTER_LINK)
-    
-    def is_logged_in(self):
-        try:
-            self.wait_for_url_contains("/")
-            return self.is_element_displayed(self.LOGOUT_BUTTON)
-        except:
-            return False
-    
-    def get_error_message(self):
-        if self.is_element_displayed(self.ERROR_MESSAGE):
-            return self.get_text(self.ERROR_MESSAGE)
-        return ""
+        with allure.step("Авторизация пользователя"):
+            self.fill_email(email)
+            self.fill_password(password)
+            self.click_login_button()
+
+    def is_logout_button_displayed(self):
+        with allure.step("Проверка отображения кнопки Выхода"):
+            return self.is_element_displayed(self.locators.LOGOUT_BUTTON)
