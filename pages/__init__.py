8a6f99c12d4196b069e9d13aed1117900d23e437
KristaDeimos <kristavoitishkina@yandex.ru>
2026-02-07 00:30:16 +0300

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,1 +1,8 @@
+from .login_page import LoginPage
 
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+    
+    def login(self, username, password):
+        return self
